7f77d34e3bb5f48fd0340c45e19117f394b6f7ff
肉小贝 <46618620+fbotp@users.noreply.github.com>
2023-11-09 12:40:35 +0800

    优化逻辑，格式化js

--- global-speed.js
+++ global-speed.js
@@ -8,82 +8,106 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  'use strict'
 
-    let currentSpeed = 1.0; // 初始速度为1.0
-    let preSpeed = 1.0;
-  
-    // 创建显示当前速度的信息框
-    const speedDisplay = document.createElement('div');
-    speedDisplay.style.position = 'fixed';
-    speedDisplay.style.top = '10px';
-    speedDisplay.style.left = '10px';
-    speedDisplay.style.padding = '10px';
-    speedDisplay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-    speedDisplay.style.color = 'white';
-    speedDisplay.style.zIndex = '9999';
-    speedDisplay.style.display = 'none';
-    speedDisplay.style.fontSize = '50px';
-    speedDisplay.textContent = currentSpeed;
-    document.body.appendChild(speedDisplay);
-    let speedDisplayTimer = null;
+  let currentSpeed = 1.0 // 初始速度为1.0
+  let preSpeed = 1.0 // 上次速度
+  let speedActionTimer = null // 短时间内多次按键避免重复设置
 
-    function updateSpeedDisplay() {
-      	speedDisplay.style.display = 'block';
-        speedDisplay.textContent = currentSpeed.toFixed(2);
-        clearTimeout(speedDisplayTimer);
-        speedDisplayTimer = setTimeout(() => {
-          speedDisplay.style.display = 'none';
-        }, 1000);
+  // 创建显示当前速度的信息框
+  const speedDisplay = document.createElement('div')
+  speedDisplay.style.position = 'fixed'
+  speedDisplay.style.top = '10px'
+  speedDisplay.style.left = '10px'
+  speedDisplay.style.padding = '10px'
+  speedDisplay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)'
+  speedDisplay.style.color = 'white'
+  speedDisplay.style.zIndex = '9999'
+  speedDisplay.style.display = 'none'
+  speedDisplay.style.fontSize = '50px'
+  speedDisplay.textContent = currentSpeed
+  document.body.appendChild(speedDisplay)
+  let speedDisplayTimer = null // 显示计时器
+
+  // 限定上下界
+  function clamp(min, max, value) {
+    let clamped = value 
+    if (min != null) {
+      clamped = Math.max(min, clamped)
     }
-
-    function changeVideoSpeed(speedChange) {
-        preSpeed = currentSpeed;
-        currentSpeed += speedChange;
-        updateSpeedDisplay();
-        const videos = document.getElementsByTagName('video');
-        for (let video of videos) {
-            video.playbackRate = currentSpeed;
-        }
+    if (max != null) {
+      clamped = Math.min(max, clamped)
     }
+    return clamped 
+  }
 
-    function toggleVideoSpeed() {
-        if (currentSpeed !== 1) {
-            preSpeed = currentSpeed;
-            currentSpeed = 1.0;
+  // 显示倍速框
+  function updateSpeedDisplay() {
+    speedDisplay.style.display = 'block'
+    speedDisplay.textContent = currentSpeed.toFixed(2)
+    clearTimeout(speedDisplayTimer)
+    speedDisplayTimer = setTimeout(() => {
+      speedDisplay.style.display = 'none'
+    }, 1000)
+  }
+
+  // 设置速度
+  function setVideoSpeed() {
+    clearTimeout(speedActionTimer)
+    speedActionTimer = setTimeout( () => {
+      const videos = document.getElementsByTagName('video')
+      for (let video of videos) {
+        try {
+          video.playbackRate = currentSpeed
+          video.defaultPlaybackRate = currentSpeed
         }
-        else {
-            currentSpeed = preSpeed;
-        }
-        updateSpeedDisplay();
-        const videos = document.getElementsByTagName('video');
-        for (let video of videos) {
-            video.playbackRate = currentSpeed;
-        }
+        catch (err) {}
+      }
+    }, 200)
+  }
+
+  // +/-速度
+  function changeVideoSpeed(speedChange) {
+    preSpeed = currentSpeed
+    currentSpeed += speedChange
+    currentSpeed = clamp(0.25, 20.00, currentSpeed)
+    setVideoSpeed()
+  }
+
+  // 当前速度与1.0切换
+  function toggleVideoSpeed() {
+    if (currentSpeed !== 1) {
+      preSpeed = currentSpeed
+      currentSpeed = 1.0
+    } else {
+      currentSpeed = preSpeed
     }
+    setVideoSpeed()
+  }
 
-    document.addEventListener('keydown', function(e) {
-        if (!['A', 'a', 'D', 'd', 'S', 's', 'Z', 'z', 'X', 'x'].includes(e.key) || ['input', 'textarea'].includes(e.target.type)) {
-            return;
-        }
-        if (['A', 'a'].includes(e.key)) {
-            changeVideoSpeed(-0.25); // 减速
-        } else if (['D', 'd'].includes(e.key)) {
-            changeVideoSpeed(0.25); // 加速
-        } else if (['S', 's'].includes(e.key)) {
-            toggleVideoSpeed(); // 切换到1.0倍速或当前倍速
-        } else if (['Z', 'z'].includes(e.key)) {
-            const videos = document.getElementsByTagName('video');
-            for (let video of videos) {
-                video.currentTime -= 10; // 快退10秒
-            }
-        } else if (['X', 'x'].includes(e.key)) {
-            const videos = document.getElementsByTagName('video');
-            for (let video of videos) {
-                video.currentTime += 10; // 快进10秒
-            }
-        }
-        e.stopPropagation();
-    });
-})();
+  document.addEventListener('keydown', function (e) {
+    if (!['A', 'a', 'D', 'd', 'S', 's', 'Z', 'z', 'X', 'x'].includes(e.key) || ['input', 'textarea'].includes(e.target.type)) {
+      return
+    }
+    if (['A', 'a'].includes(e.key)) {
+      changeVideoSpeed(-0.25) // 减速
+    } else if (['D', 'd'].includes(e.key)) {
+      changeVideoSpeed(0.25) // 加速
+    } else if (['S', 's'].includes(e.key)) {
+      toggleVideoSpeed() // 切换到1.0倍速或当前倍速
+    } else if (['Z', 'z'].includes(e.key)) {
+      const videos = document.getElementsByTagName('video')
+      for (let video of videos) {
+        video.currentTime -= 10 // 快退10秒
+      }
+    } else if (['X', 'x'].includes(e.key)) {
+      const videos = document.getElementsByTagName('video')
+      for (let video of videos) {
+        video.currentTime += 10 // 快进10秒
+      }
+    }
+    updateSpeedDisplay()
+    e.stopPropagation()
+  })
+})()
